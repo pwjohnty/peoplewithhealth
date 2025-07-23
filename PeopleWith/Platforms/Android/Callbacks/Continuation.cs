fad233abcfae59e173c520c9992c2a2487201951
Mark McCloskey <mark.mccloskey@peoplewith.com>
2025-07-23 09:02:34 +0100

    Android Fitness data code added

---
+++ PeopleWith/Platforms/Android/Callbacks/Continuation.cs
@@ -0,0 +1,44 @@
+﻿using Android.Runtime;
+using Kotlin.Coroutines;
+using Java.Lang;
+using AndroidX.Health.Connect.Client;
+using AndroidX.Health.Connect.Client.Aggregate;
+
+namespace PeopleWith.Platforms.Android.Callbacks
+{
+    internal class Continuation : Java.Lang.Object, IContinuation
+    {
+        public ICoroutineContext Context => EmptyCoroutineContext.Instance;
+
+        private readonly TaskCompletionSource<Java.Lang.Object> _taskCompletionSource;
+
+
+
+        public Continuation(TaskCompletionSource<Java.Lang.Object> taskCompletionSource, CancellationToken cancellationToken)
+        {
+            _taskCompletionSource = taskCompletionSource;
+            cancellationToken.Register(() => _taskCompletionSource.TrySetCanceled());
+        }
+
+
+        public void ResumeWith(Java.Lang.Object result)
+        {
+
+
+            //Check if there are any exception. We don't have access to the class Kotlin.Result.Failure. But we can extraxt the exception (Throwable) from the field in the class.
+            var exceptionField = result.Class.GetDeclaredFields().FirstOrDefault(x => x.Name.Contains("exception", StringComparison.OrdinalIgnoreCase));
+            if (exceptionField != null)
+            {
+                Throwable exception = exceptionField.Get(result).JavaCast<Java.Lang.Throwable>();
+                _taskCompletionSource.TrySetException(new System.Exception(exception.Message));
+                return;
+            }
+            else
+            {
+                _taskCompletionSource.TrySetResult(result);
+            }
+
+
+        }
+    }
+}
---
+++ PeopleWith/Platforms/Android/Permissions/PermissionHandler.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.Util;
+using AndroidX.Core.App;
+using PeopleWith.Platforms.Android.Callbacks;
+using Java.Util;
+using PeopleWith.Platforms.Android.Callbacks;
+using PeopleWith;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JObject = Java.Lang.Object;
+
+namespace PeopleWith.Platforms.Android.Permissions
+{
+    internal class PermissionHandler
+    {
+        public static async Task<List<string>> Request(Java.Lang.Object permissions, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+
+                if (Platform.CurrentActivity is not MainActivity activity)
+                    return null;
+
+                var whenCompletedSource = new TaskCompletionSource<JObject?>();
+                _ = Task.Delay(MainActivity.MaxPermissionRequestDuration, cancellationToken)
+                    .ContinueWith(_ => whenCompletedSource.TrySetResult(null), TaskScheduler.Default);
+
+
+                new AlertDialog.Builder(activity)
+                    .SetTitle("Health connect permission isn't granted")
+                    .SetMessage("Do you want to allow permissions?")
+                    .SetNegativeButton("Decline", (_, _) => whenCompletedSource.TrySetResult(null))
+                    .SetPositiveButton("Allow", (_, _) => RequestPermission())
+                    .Show();
+
+
+                JObject? result = await whenCompletedSource.Task.ConfigureAwait(false);
+                //return (ISet)result;
+                return KotlinCallback.ConvertISetToList((ISet)result);
+
+                void RequestPermission()
+                => activity.RequestPermission(permissions, whenCompletedSource);
+            }
+            catch (Exception ex)
+            {
+                return null;
+                // Handle exceptions as needed, e.g., logging or user notification
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
