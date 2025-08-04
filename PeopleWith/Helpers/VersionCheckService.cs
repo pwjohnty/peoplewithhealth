5aa85f7aa400faf733d04cded2490ff1a43df71b
Conal <Conal.Fullen@peoplewith.com>
2025-08-04 14:34:26 +0100

    Fix for closing app after registering email and password

--- PeopleWith/Helpers/VersionCheckService.cs
+++ PeopleWith/Helpers/VersionCheckService.cs
@@ -17,7 +17,7 @@ namespace PeopleWith
             }
             else
             {
-                latestVersion = "98.1";
+                latestVersion = "99.1";
             }
 
             if (string.IsNullOrEmpty(currentVersion) || string.IsNullOrEmpty(latestVersion)) return false;
